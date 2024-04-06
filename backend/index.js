45c60629de5023113214f7d1133c0b2f8c33281e
root <root@ip-172-31-1-78.us-east-2.compute.internal>
2024-04-06 09:20:57 +0000

    Added HTTPS Security and SSL: ssl/certificate.crt, ssl/private.key and B5********.txt for verifying ZEROSSL

--- backend/index.js
+++ backend/index.js
@@ -8,7 +8,18 @@ const authenticateUser = require('./authMiddleware/jwtAuth')
 // Create an instance of express
 const bodyParser = require('body-parser');
 const app = express();
+const path = require('path');
+const fs = require('fs');
+const https = require('https');
 require('./config/database').connect();
+const key = fs.readFileSync('./ssl/private.key');
+const cert = fs.readFileSync('./ssl/certificate.crt');
+
+const creds = {
+	key,
+	cert
+}
+
 app.use(bodyParser.json({ limit: '50mb' }));
 app.use(bodyParser.urlencoded({ extended: true, limit: '50mb' }));
 app.use(express.json());
@@ -24,10 +35,17 @@ app.get('/test',(req,res)=>{
 	res.send('This is test')
 }
 )
+//Verification SSL
+app.get('/.well-known/pki-validation/B5ED39FF81F2D0D4C59964DF6AA8F00B.txt', (req,res)=>{
+	res.sendFile(path.join(__dirname,'B5ED39FF81F2D0D4C59964DF6AA8F00B.txt'));
+})
 app.use('/onlyvocal', authenticateUser, userRouter);
 app.use('/auth',  authRouter)
 app.use('/protected', authenticateUser, protectedRouter)
-const PORT = process.env.PORT || 5175;
+const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+
+const httpsServer = https.createServer(creds,app);
+httpsServer.listen(5175);
